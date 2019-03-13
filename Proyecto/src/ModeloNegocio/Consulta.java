579ad73e2c246950e0e80c58476eb33373573d6b
Jonathan Andres Jimenez Trujillo <jonajitru781@gmail.com>
2019-03-13 14:30:53 -0500

    Update Consulta.java

--- Proyecto/src/ModeloNegocio/Consulta.java
+++ Proyecto/src/ModeloNegocio/Consulta.java
@@ -10,5 +10,25 @@ package ModeloNegocio;
  * @author Lenovo
  */
 public class Consulta {
+    private String consulta;
+
+    public Consulta(String consulta) {
+        this.consulta = consulta;
+    }
+       
+    public String getStringConsulta(String nombreTabla){
+        return this.consulta="SELECT * FROM "+nombreTabla;
+    }
     
+    public String getStringConsulta(String nombreTabla, String[] nombreAtributos, String[] condiciones, String condicional){
+        String valorConsulta=null;
+        if (nombreAtributos.length==condiciones.length) {
+            valorConsulta="SELECT * FROM "+nombreTabla+" WHERE ";
+            for (int i = 0; i < nombreAtributos.length-1 ; i++){
+                valorConsulta=valorConsulta+" "+nombreTabla+"."+nombreAtributos[i]+"='"+condiciones[i]+"' "+condicional;
+            }
+            valorConsulta=valorConsulta+" "+nombreTabla+"."+nombreAtributos[nombreAtributos.length]+"='"+condiciones[condiciones.length]+"'";
+        }
+        return valorConsulta;
+    }
 }
