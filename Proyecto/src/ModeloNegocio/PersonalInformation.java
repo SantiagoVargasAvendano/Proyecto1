2d305a82255308675420a3399c8076dd361866b3
Jonathan Andres Jimenez Trujillo <32826431+jonajitru781@users.noreply.github.com>
2019-03-13 11:32:55 -0500

    Update

--- Proyecto/src/ModeloNegocio/PersonalInformation.java
+++ Proyecto/src/ModeloNegocio/PersonalInformation.java
@@ -32,8 +32,6 @@ public class PersonalInformation {
     private String numberEmergencyContact;
     private String emailEmergencyContact;
     private String relationship;
-    private String photo;
-
 
     public PersonalInformation(String fullName, String typeDocument, String Id, String gender, 
             String dateBirth, String mobile, String email, String sizeShirt, 
@@ -60,18 +58,174 @@ public class PersonalInformation {
         this.relationship = relationship;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getTypeDocument() {
+        return typeDocument;
+    }
+
+    public void setTypeDocument(String typeDocument) {
+        this.typeDocument = typeDocument;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getCitizen() {
+        return citizen;
     }
 
     public void setCitizen(String citizen) {
         this.citizen = citizen;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
     public void setPassport(String passport) {
         this.passport = passport;
     }
-    
+
+    public String getSizeShirt() {
+        return sizeShirt;
+    }
+
+    public void setSizeShirt(String sizeShirt) {
+        this.sizeShirt = sizeShirt;
+    }
+
+    public String getCurrentOcupation() {
+        return currentOcupation;
+    }
+
+    public void setCurrentOcupation(String currentOcupation) {
+        this.currentOcupation = currentOcupation;
+    }
+
+    public String getFieldsStudy() {
+        return fieldsStudy;
+    }
+
+    public void setFieldsStudy(String fieldsStudy) {
+        this.fieldsStudy = fieldsStudy;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public boolean isGraduate() {
+        return graduate;
+    }
+
+    public void setGraduate(boolean graduate) {
+        this.graduate = graduate;
+    }
+
+    public String getCurrentAdress() {
+        return currentAdress;
+    }
+
+    public void setCurrentAdress(String currentAdress) {
+        this.currentAdress = currentAdress;
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
+    }
+
+    public void setCurrentCity(String currentCity) {
+        this.currentCity = currentCity;
+    }
+
+    public String getFullNameEmergencyContact() {
+        return fullNameEmergencyContact;
+    }
+
+    public void setFullNameEmergencyContact(String fullNameEmergencyContact) {
+        this.fullNameEmergencyContact = fullNameEmergencyContact;
+    }
+
+    public String getNumberEmergencyContact() {
+        return numberEmergencyContact;
+    }
+
+    public void setNumberEmergencyContact(String numberEmergencyContact) {
+        this.numberEmergencyContact = numberEmergencyContact;
+    }
+
+    public String getEmailEmergencyContact() {
+        return emailEmergencyContact;
+    }
+
+    public void setEmailEmergencyContact(String emailEmergencyContact) {
+        this.emailEmergencyContact = emailEmergencyContact;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+
     
     
     
