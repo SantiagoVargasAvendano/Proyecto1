b085d3a536ae6f0bcd2e802e07a0611d3f11e1f1
Jonathan Andres Jimenez Trujillo <jonajitru781@gmail.com>
2019-03-14 00:32:42 -0500

    BD en GestorPlataforma

--- Proyecto/src/ModeloNegocio/GestorPlataforma.java
+++ Proyecto/src/ModeloNegocio/GestorPlataforma.java
@@ -6,12 +6,28 @@
 package ModeloNegocio;
 
 import java.util.ArrayList;
-
+import com.healthmarketscience.jackcess.Column;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Lenovo
  */
 public class GestorPlataforma {
+    private Database bd;
     private ArrayList<PersonalInformation> personas;
     private ArrayList<ProgramsCampsColombia> programasColombia;
     private ArrayList<VolunteerPrograms> programasVoluntariado;
@@ -19,6 +35,13 @@ public class GestorPlataforma {
     private ArrayList<ProgramsAssignment> programasColombiaAssignment;
     private ArrayList<ICCPAssignment> ICCPAssignments;
     private ArrayList<LocalVolunteerAssignment> volunteerAssignment;
+    private Table personaInformation;
+    private Table campsICCP;
+    private Table ICCPAssignment;
+    private Table programasBDColombia;
+    private Table programsAssignment;
+    private Table programasBDVoluntariado;
+    private Table voluntariadoAssignment;
 
     public GestorPlataforma() {
         this.personas = new ArrayList<>();
@@ -28,9 +51,43 @@ public class GestorPlataforma {
         this.programasColombiaAssignment = new ArrayList<>();
         this.ICCPAssignments = new ArrayList<>();
         this.volunteerAssignment = new ArrayList<>();
+        try {
+            this.bd = DatabaseBuilder.open(new File("bd proyecto final.accdb"));
+            Table personaInformation = bd.getTable("Personal Information");
+            Table campsICCP = bd.getTable("Camps ICCP");
+            Table ICCPAssignment = bd.getTable("ICCP Assignment");
+            Table programasBDColombia = bd.getTable("Programs - Camps Colombia");
+            Table programsAssignment= bd.getTable("Programs Assignment");
+            Table programasBDVoluntariado = bd.getTable("Volunteer Programs");
+            Table voluntariadoAssignment = bd.getTable("Local Volunteer Programs Assignment");  
+        } catch (IOException ex) {
+            Logger.getLogger(GestorPlataforma.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public boolean addPersona(PersonalInformation persona){
+        String fullName=persona.getFullName();
+        String typeDocument=persona.getTypeDocument();
+        String Id=persona.getId();
+        String gender=persona.getGender();
+        String dateBirth=persona.getDateBirth();
+        String mobile=persona.getMobile();
+        String phone=persona.getPhone(); 
+        String citizen=persona.getCitizen();    
+        String email=persona.getEmail();
+        String passport=persona.getPassport();
+        String sizeShirt=persona.getSizeShirt();
+        String currentOcupation=persona.getCurrentOcupation();
+        String fieldsStudy=persona.getFieldsStudy();
+        String university=persona.getUniversity();
+        boolean graduate=persona.isGraduate();
+        String currentAdress=persona.getCurrentAdress();
+        String currentCity=persona.getCurrentCity();
+        String fullNameEmergencyContact=persona.getFullNameEmergencyContact();
+        String numberEmergencyContact=persona.getNumberEmergencyContact();
+        String emailEmergencyContact=persona.getEmailEmergencyContact();
+        String relationship=persona.getRelationship();
+        
         return this.personas.add(persona);
     }
     
@@ -187,6 +244,200 @@ public class GestorPlataforma {
             this.programasColombiaAssignment.add(ProgramsAssignBd.get(i));
         }
         return true;
+    }
+    
+    public ArrayList<PersonalInformation> getPersona() throws IOException{
+        ArrayList<PersonalInformation> persona=new ArrayList<>();
+        Table personaInformation = bd.getTable("Personal Information");
+        String fullName;
+        String typeDocument;
+        String Id;
+        String gender;
+        String dateBirth;
+        String mobile;
+        String phone; 
+        String citizen;    
+        String email;
+        String passport;
+        String sizeShirt;
+        String currentOcupation;
+        String fieldsStudy;
+        String university;
+        boolean graduate;
+        String currentAdress;
+        String currentCity;
+        String fullNameEmergencyContact;
+        String numberEmergencyContact;
+        String emailEmergencyContact;
+        String relationship;
+        for(Row row : personaInformation) {
+            fullName =row.getString("Full Name");
+            typeDocument=row.getString("Type of Document");
+            Id=row.getString("ID");
+            gender=row.getString("Gender");
+            dateBirth=row.getString("Date of Birth");
+            mobile=row.getString("Mobile");
+            phone=row.getString("Phone");
+            citizen=row.getString("Citizen");
+            email=row.getString("E-Mail");
+            sizeShirt=row.getString("Size of Shirt");
+            currentOcupation=row.getString("Current Ocupation");
+            fieldsStudy=row.getString("Field of Study");
+            university=row.getString("University");
+            graduate=row.getBoolean("Graduate");
+            currentAdress=row.getString("Current Adress");
+            currentCity=row.getString("Current City");
+            fullNameEmergencyContact=row.getString("Full Name Emergency Contact");
+            numberEmergencyContact=row.getString("Number Emergency Contact");
+            emailEmergencyContact=row.getString("Email Emergency Contact");
+            relationship=row.getString("Relationship");
+            passport=row.getString("Passport");
+            PersonalInformation personaAgregar=new PersonalInformation(fullName, typeDocument, Id, gender, dateBirth, mobile, email, sizeShirt, currentOcupation, fieldsStudy, university, graduate, currentAdress, currentCity, fullNameEmergencyContact, numberEmergencyContact, emailEmergencyContact, relationship);
+            personaAgregar.setPassport(passport);
+            persona.add(personaAgregar);
+        }
+        return persona;
+    }
+    
+    public ArrayList<CampsICCP> getCampamentosICCP() throws IOException{
+        ArrayList<CampsICCP> iccp=new ArrayList<>();
+        String nombre;
+        String codigo;
+        String year;
+        String direccion;
+        String campyear;
+        Table campsICCP = bd.getTable("Camps ICCP");
+        for(Row row : campsICCP) {
+            nombre=row.getString("Nombre");
+            codigo=row.getString("Codigo/ Camp ICCP ID");
+            year=row.getString("Año");
+            direccion=row.getString("Direccion");
+            campyear=row.getString("Camp year");
+            CampsICCP Agregar=new CampsICCP(year, direccion,codigo,nombre);
+            Agregar.setCampYear(campyear);
+            iccp.add(Agregar);
+        }
+        return iccp;
+    }
+    
+    public ArrayList<ICCPAssignment> getAssignmentICCP() throws IOException{
+        ArrayList<ICCPAssignment> iccp=new ArrayList<>();
+        String nombreParticipante;
+        String codigoParticipante;
+        String codigoCampamento;
+        String fechaInicio;
+        String fechaFin;
+        String role;
+        String calificacion;
+        String note;
+        Table ICCPAssignment = bd.getTable("ICCP Assignment");
+        for(Row row : ICCPAssignment) {
+            nombreParticipante=row.getString("Nombre Participante/ ID Participant");
+            codigoParticipante=row.getString("ID");
+            codigoCampamento=row.getString("Codigo/ Camp ICCP ID");
+            fechaInicio=row.getString("Fecha inicio");
+            fechaFin=row.getString("Fecha fin");
+            role=row.getString("Role");
+            calificacion=row.getString("Calification");
+            note=row.getString("Note");
+            ICCPAssignment Agregar=new ICCPAssignment(nombreParticipante, codigoParticipante, codigoCampamento, fechaInicio, fechaFin, role, calificacion, note);
+            iccp.add(Agregar);
+        }
+        return iccp;
+    }
+    
+    public ArrayList<ProgramsAssignment> getAssignmentColombia() throws IOException{
+        ArrayList<ProgramsAssignment> asignacionProgramas=new ArrayList<>();
+        String nombreParticipante;
+        String codigoParticipante;
+        String codigoCampamento;
+        String role;
+        String calificacion;
+        String note;
+        Table programsAssignment= bd.getTable("Programs Assignment");
+        for(Row row : programsAssignment) {
+            nombreParticipante=row.getString("Personal ID");
+            codigoParticipante=row.getString("ID");
+            codigoCampamento=row.getString("Camp ID");
+            role=row.getString("Role");
+            calificacion=row.getString("Calification");
+            note=row.getString("Note");
+            ProgramsAssignment Agregar=new ProgramsAssignment(codigoParticipante,nombreParticipante, codigoCampamento, role, calificacion, note);
+            asignacionProgramas.add(Agregar);
+        }
+        return asignacionProgramas;
+    }
+    
+    public ArrayList<ProgramsCampsColombia> getCampsColombia() throws IOException{
+        ArrayList<ProgramsCampsColombia> campamentosColombia=new ArrayList<>();
+        String campID;
+        String nameCamp;
+        String tipoPrograma;
+        String company;
+        String campDate;
+        String duration;
+        String placeDeveloped;
+        String population;
+        Table programasColombia = bd.getTable("Programs - Camps Colombia");
+        for(Row row : programasColombia) {
+            campID=row.getString("Camp ID");
+            nameCamp=row.getString("Name Camp");
+            tipoPrograma=row.getString("Type of Program");
+            company=row.getString("Company");
+            campDate=row.getString("Camp Date");
+            duration=row.getString("Duration");
+            placeDeveloped=row.getString("Place developed");
+            population=row.getString("Population");
+            ProgramsCampsColombia Agregar=new ProgramsCampsColombia(tipoPrograma, company, campDate, duration, placeDeveloped, population, campID);
+            Agregar.setNombre(nameCamp);
+            campamentosColombia.add(Agregar);
+        }
+        return campamentosColombia;
+    }
+    
+    public ArrayList<VolunteerPrograms> getVoluntariado() throws IOException{
+        ArrayList<VolunteerPrograms> voluntariado=new ArrayList<>();
+        String voluntariadoID;
+        String nameProgram;
+        String tipoVoluntariado;
+        String company;
+        String fechaGrado;
+        String duration;
+        Table programasVoluntariado = bd.getTable("Volunteer Programs");
+        for(Row row : programasVoluntariado) {
+            voluntariadoID=row.getString("Voluntariado ID");
+            nameProgram=row.getString("Name of Program");
+            company=row.getString("Company");
+            fechaGrado=row.getString("Fecha Grado");
+            duration=row.getString("Duration");
+            tipoVoluntariado=row.getString("Tipo Voluntariado");
+            VolunteerPrograms Agregar=new VolunteerPrograms(company, fechaGrado, duration, tipoVoluntariado, voluntariadoID);
+            Agregar.setNombre(nameProgram);
+            voluntariado.add(Agregar);
+        }
+        return voluntariado;
+    }
+    
+    public ArrayList<LocalVolunteerAssignment> getAssignmentVoluntariado() throws IOException{
+        ArrayList<LocalVolunteerAssignment> asignacionVoluntariado=new ArrayList<>();
+        String nombreVoluntario;
+        String codigoVoluntario;
+        String codigoVoluntariado;
+        String role;
+        String calificacion;
+        String note;
+        Table voluntariadoAssignment = bd.getTable("Local Volunteer Programs Assignment");
+        for(Row row : voluntariadoAssignment) {
+            nombreVoluntario=row.getString("Volunteer ID");
+            codigoVoluntario=row.getString("Volunteer Name");
+            codigoVoluntariado=row.getString("Voluntariado ID");
+            role=row.getString("Role");
+            calificacion=row.getString("Calification");
+            note=row.getString("Note");
+            LocalVolunteerAssignment Agregar=new LocalVolunteerAssignment(codigoVoluntario, nombreVoluntario, codigoVoluntariado, role, calificacion, note);
+            asignacionVoluntariado.add(Agregar);
+        }
+        return asignacionVoluntariado;
     }
     
 }
